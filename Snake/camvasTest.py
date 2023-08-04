cb9646d394cd86fc84f1afbd2b2c188b58fcd29f
Magnus Fresvik <magnus.fresvik@gmail.com>
2023-08-04 16:19:38 +0200

    teit kode, men funker

--- Snake/camvasTest.py
+++ Snake/camvasTest.py
@@ -19,24 +19,103 @@ for i in range(1,grid_resolution):
 #snakeshape
 
 #initial position
-if "head_x" not in locals():
-    head_x = window_size/grid_resolution
-    head_y = window_size/grid_resolution
+player_x = window_size / grid_resolution
+player_y = window_size / grid_resolution
 
-board.create_rectangle(head_x,head_y,head_x+(window_size/grid_resolution),head_y+(window_size/grid_resolution), fill= "green")
+player_shape = board.create_rectangle(player_x, player_y, player_x + (window_size / grid_resolution),
+                                      player_y + (window_size / grid_resolution), fill="green")
+
+# variables to track movement
+move_direction = "right"
+
+def move_loop():
+    global move_direction
+    if move_direction == "right":
+        moveRight()
+    elif move_direction == "left":
+        moveLeft()
+    elif move_direction == "up":
+        moveUp()
+    elif move_direction == "down":
+        moveDown()
+    root.after(200, move_loop)
+
+def input_handler(event):
+    global move_direction
+
+    if event.char == "d":
+        move_direction = "right"
+    elif event.char == "a":
+        move_direction = "left"
+    elif event.char == "w":
+        move_direction = "up"
+    elif event.char == "s":
+        move_direction = "down"
 
 #controls
-def moveRight (event):
-            global head_x
-            time.sleep(0.2)
-            head_x += window_size/grid_resolution
-            root.after(200,moveRight)
-            print("right")
-            print("{}".format(event))
+def moveRight():
+    global player_x
+    global board
+    
+    next_x = player_x + (window_size / grid_resolution)
+    if next_x <= window_size - (window_size / grid_resolution):
+        player_x = next_x
+    else:
+        first_x = window_size / grid_resolution
+        player_x = first_x - 1
 
-root.bind("<KeyPress-d>", moveRight )
+    board.coords(player_shape, player_x, player_y, player_x + (window_size / grid_resolution),
+            player_y + (window_size / grid_resolution))
+
+    print("Moved right")
+
+def moveLeft():
+    global player_x
+
+    next_x = player_x - (window_size / grid_resolution)
+    if next_x >= 0:
+        player_x = next_x
+    else:
+        last_x = window_size - (window_size / grid_resolution)
+        player_x = last_x + 1
+
+    board.coords(player_shape, player_x, player_y, player_x + (window_size / grid_resolution),
+            player_y + (window_size / grid_resolution))
+
+    print("Moved left")
+
+def moveUp():
+    global player_y
+
+    next_y = player_y - (window_size / grid_resolution)
+    if next_y >= 0:
+        player_y = next_y
+    else:
+        last_y = window_size - (window_size / grid_resolution)
+        player_y = last_y + 1
+
+    board.coords(player_shape, player_x, player_y, player_x + (window_size / grid_resolution),
+            player_y + (window_size / grid_resolution))
+
+    print("Moved up")
+
+def moveDown():
+    global player_y
+
+    next_y = player_y + (window_size / grid_resolution)
+    if next_y <= window_size - (window_size / grid_resolution):
+        player_y = next_y
+    else:
+        first_y = window_size / grid_resolution
+        player_y = first_y - 1
+
+    board.coords(player_shape, player_x, player_y, player_x + (window_size / grid_resolution),
+            player_y + (window_size / grid_resolution))
+
+    print("Moved down")
+
+root.bind("<KeyPress>", input_handler )
 
 board.pack()
-
-
+root.after(200, move_loop)
 root.mainloop()
